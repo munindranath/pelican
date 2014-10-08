e89e809872cdf52a6ed8404a20865834375866e6
munindranath <munindra.nath@gmail.com>
2014-10-08 18:31:57 -0400

    new commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,7 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
-THEME = "pelican-themes-master\mnmlist"
+THEME = "pelican-themes-master/basic"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
